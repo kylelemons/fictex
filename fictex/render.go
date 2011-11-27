c784ac75d77137e18cb433a7327890c199532d67
Kyle Lemons <kyle@kylelemons.net>
2011-11-27 12:20:33 -0800

    add renderer

---
+++ fictex/render.go
@@ -0,0 +1,101 @@
+package fictex
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+type StringPair [2]string
+
+type Renderer struct {
+	// The following are used to bracket the appropriate text
+	Bold StringPair
+	Slant StringPair
+	Underline StringPair
+	Paragraph StringPair
+
+	// The following are used in place of the corresponding node
+	NDash string
+	MDash string
+	HLine string
+
+	// The first of the pair will be formatted with Sprintf(fmt, preview)
+	Preview StringPair
+}
+
+var TextRenderer = Renderer{
+	Bold: StringPair{"*", "*"},
+	Slant: StringPair{"/", "/"},
+	Underline: StringPair{"_", "_"},
+	Paragraph: StringPair{"\n    ", "\n"},
+
+	NDash: "--",
+	MDash: "---",
+	HLine: "\n-----\n",
+
+	Preview: StringPair{"<<%s\n", "\n>>\n"},
+}
+
+var HTMLRenderer = Renderer{
+	Bold: StringPair{"<b>", "</b>"},
+	Slant: StringPair{"<i>", "</i>"},
+	Underline: StringPair{"<u>", "</u>"},
+	Paragraph: StringPair{"<p>\n", "\n</p>\n"},
+
+	NDash: "&ndash;",
+	MDash: "&mdash;",
+	HLine: "\n<hl>\n",
+
+	Preview: StringPair{"<!-- Preview: %q -->\n", "\n<!-- /Preview -->\n"},
+}
+
+func (r Renderer) Render(w io.Writer, n Node) os.Error {
+	var render func(Node) os.Error
+	render = func(n Node) (err os.Error) {
+		switch n.Type {
+			case Group:
+				for _, n := range n.Child {
+					if err := render(n); err != nil {
+						return err
+					}
+				}
+			case Text:
+				_, err = w.Write(n.Text)
+			case Bold:
+				_, err = fmt.Fprintf(w, "%s%s%s", r.Bold[0], n.Text, r.Bold[1])
+			case Slant:
+				_, err = fmt.Fprintf(w, "%s%s%s", r.Slant[0], n.Text, r.Slant[1])
+			case Underline:
+				_, err = fmt.Fprintf(w, "%s%s%s", r.Underline[0], n.Text, r.Underline[1])
+			case Paragraph:
+				if _, err := io.WriteString(w, r.Paragraph[0]); err != nil {
+					return err
+				}
+				for _, n := range n.Child {
+					render(n)
+				}
+				_, err = io.WriteString(w, r.Paragraph[1])
+			case NDash:
+				_, err = io.WriteString(w, r.NDash)
+			case MDash:
+				_, err = io.WriteString(w, r.MDash)
+			case HLine:
+				_, err = io.WriteString(w, r.HLine)
+			case Preview:
+				if _, err := fmt.Fprintf(w, r.Preview[0], n.Text); err != nil {
+					return err
+				}
+				for _, n := range n.Child {
+					if err := render(n); err != nil {
+						return err
+					}
+				}
+				_, err = io.WriteString(w, r.Preview[1])
+			default:
+				_, err = fmt.Fprintf(w, "Unhandled %s\n", n)
+		}
+		return err
+	}
+	return render(n)
+}
---
+++ fictex/render_test.go
@@ -0,0 +1,109 @@
+package fictex
+
+import (
+	"bytes"
+	"testing"
+)
+
+var renderTests = []struct{
+	Desc  string
+	Input Node
+	Text  string
+	HTML  string
+}{
+	{
+		Desc: "Basic test",
+		Input: Node{
+			Type: Text,
+			Text: []byte("test"),
+		},
+		Text: "test",
+		HTML: "test",
+	},
+	{
+		Desc: "Group",
+		Input: Node{
+			Type: Group,
+			Child: []Node{{
+				Type: Text,
+				Text: []byte("a"),
+			}, {
+				Type: Text,
+				Text: []byte("b"),
+			}},
+		},
+		Text: "ab",
+		HTML: "ab",
+	},
+	{
+		Desc: "Wrapping",
+		Input: Node{
+			Type: Paragraph,
+			Child: []Node{{
+				Type: Bold,
+				Text: []byte("a"),
+			}, {
+				Type: Slant,
+				Text: []byte("b"),
+			}, {
+				Type: Underline,
+				Text: []byte("c"),
+			}},
+		},
+		Text: "\n    *a*/b/_c_\n",
+		HTML: "<p>\n<b>a</b><i>b</i><u>c</u>\n</p>\n",
+	},
+	{
+		Desc: "Dashes",
+		Input: Node{
+			Type: Group,
+			Child: []Node{{
+				Type: NDash,
+			}, {
+				Type: Text,
+				Text: []byte(" "),
+			}, {
+				Type: MDash,
+			}, {
+				Type: HLine,
+			}},
+		},
+		Text: "-- ---\n-----\n",
+		HTML: "&ndash; &mdash;\n<hl>\n",
+	},
+	{
+		Desc: "Preview",
+		Input: Node{
+			Type: Preview,
+			Text: []byte("short"),
+			Child: []Node{{
+				Type: Text,
+				Text: []byte("long"),
+			}},
+		},
+		Text: "<<short\nlong\n>>\n",
+		HTML: "<!-- Preview: \"short\" -->\nlong\n<!-- /Preview -->\n",
+	},
+}
+
+func TestRender(t *testing.T) {
+	for _, test := range renderTests {
+		desc := test.Desc
+		b := new(bytes.Buffer)
+
+		if err := TextRenderer.Render(b, test.Input); err != nil {
+			t.Fatalf("%s: rendertext: %s", desc, err)
+		}
+		if got, want := b.String(), test.Text; got != want {
+			t.Errorf("%s: rendertext = %q, want %q", desc, got, want)
+		}
+
+		b.Truncate(0)
+		if err := HTMLRenderer.Render(b, test.Input); err != nil {
+			t.Fatalf("%s: renderhtml: %s", desc, err)
+		}
+		if got, want := b.String(), test.HTML; got != want {
+			t.Errorf("%s: renderhtml = %q, want %q", desc, got, want)
+		}
+	}
+}
