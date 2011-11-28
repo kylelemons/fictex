05fa7bca054732e767e18b5f4c6e33fc3f2ab94b
Kyle Lemons <kyle@kylelemons.net>
2011-11-27 20:27:19 -0800

    fix testdata

--- ui/main.go
+++ ui/main.go
@@ -8,11 +8,8 @@ import (
 	"appengine"
 )
 
-func Root(c appengine.Context, w http.ResponseWriter, r *http.Request) os.Error {
-	set, err := template.ParseTemplateGlob("templates/*.html")
-	if err != nil {
-		return err
-	}
+var templates = template.SetMust(template.ParseTemplateGlob("templates/*.html"))
 
-	return set.Execute(w, "main.html", nil)
+func Root(c appengine.Context, w http.ResponseWriter, r *http.Request) os.Error {
+	return templates.Execute(w, "main.html", nil)
 }
